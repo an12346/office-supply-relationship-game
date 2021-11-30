f3f0f4a633336aa0f969caf8bc4bb42217e47f3a
Anna Pittman <annaepittman@gmail.com>
2021-11-30 14:07:04 -0800

    edit index.html, edit index.js
    Co-Authored-by: Albert Lee <leesga8@gmail.com>
    Co-Authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-Authored-by: Colt Thatcher <colt.thatcher303@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,57 +3,69 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-let staple = 0;
-let sticky = 0;
-let trash = 0;
+$(document).ready(function () {
 
-function calculateWinner() {
-  if (trash === staple || trash === sticky) {
-    trash += 1;
-  } else if (sticky === staple) {
-    sticky += 1;
-  }  
+  let staple = 0;
+  let sticky = 0;
+  let trash = 0;
+  let userName = $('#userName').val();
 
-  if (trash > sticky && trash > staple) {
-    console.log(trash);
-    $('#trash').show();
-  } else if (sticky > trash && sticky > staple) {
-    console.log(sticky);
-    $('#sticky').show();
-  } else if (staple > trash && staple > sticky) {
-    console.log(staple);
-    $('#stapler').show();
+  function calculateWinner() {
+    if (trash === staple || trash === sticky) {
+      trash += 1;
+    } else if (sticky === staple) {
+      sticky += 1;
+    }
+
+    if (trash > sticky && trash > staple) {
+      $('#trashImg').show();
+    } else if (sticky > trash && sticky > staple) {
+      $('#stickyImg').show();
+    } else if (staple > trash && staple > sticky) {
+      $('#staplerImg').show();
+    }
+
   }
 
-}
-function firstq() {
-  $('#question1').hide();
-  $('#question2').show();
-}
+  function firstq() {
+    $('#question1').hide();
+    $('#question2').show();
+  }
 
-function secq() {
-  $('#question2').hide();
-  $('#question3').show();
-}
+  function secq() {
+    $('#question2').hide();
+    $('#question3').show();
+  }
 
-function thirdq() {
-  $('#question3').hide();
-  $('#question4').show();
-}
+  function thirdq() {
+    $('#question3').hide();
+    $('#question4').show();
+  }
 
-function fourthq() {
-  $('#question4').hide();
-  $('#question5').show();
-}
+  function fourthq() {
+    $('#question4').hide();
+    $('#question5').show();
+  }
 
-function fifthQ() {
-  $('#question5').hide();
-  calculateWinner();
-}
+  function fifthQ() {
+    $('#question5').hide();
+    $('#question6').show();
+  }
 
+  function sixthQ() {
+    $('#question6').hide();
+    $('#question7').show();
+  }
 
+  function seventhQ() {
+    $('#question7').hide();
+    $('#question8').show();
+  }
 
-$(document).ready(function () {
+  function eighthQ() {
+    $('#question8').hide();
+    calculateWinner();
+  }
 
   //START PLAY PHASE
   $('#play').click(function () {
@@ -117,22 +129,70 @@ $(document).ready(function () {
     sticky += 1;
     fourthq();
   });
- 
+
   //FIFTH QUESTION PHASE
   $('.5buttonStapler').click(function () {
     staple += 1;
-    console.log(staple);
     fifthQ();
   });
   $('.5buttonTrash').click(function () {
     trash += 1;
-    console.log(trash);
     fifthQ();
   });
   $('.5buttonSticky').click(function () {
     sticky += 1;
-    console.log(sticky);
     fifthQ();
+  });
+
+  //SIXTH QUESTION PHASE
+  $('.6buttonStapler').click(function () {
+    staple += 1;
+    sixthQ();
+  });
+  $('.6buttonTrash').click(function () {
+    trash += 1;
+    sixthQ();
+  });
+  $('.6buttonSticky').click(function () {
+    sticky += 1;
+    sixthQ();
+  });
+
+  //SEVENTH QUESTION PHASE
+  $('.7buttonStapler').click(function () {
+    staple += 1;
+    seventhQ();
+  });
+  $('.7buttonTrash').click(function () {
+    trash += 1;
+    seventhQ();
+  });
+  $('.7buttonSticky').click(function () {
+    sticky += 1;
+    seventhQ();
+  });
+
+  //EIGHT QUESTION PHASE
+  $('.8buttonStapler').click(function () {
+    staple += 1;
+    console.log("St: " + staple);
+    console.log("T: " + trash);
+    console.log("St: " + sticky);
+    eighthQ();
+  });
+  $('.8buttonTrash').click(function () {
+    trash += 1;
+    console.log("St: " + staple);
+    console.log("T: " + trash);
+    console.log("St: " + sticky);
+    eighthQ();
+  });
+  $('.8buttonSticky').click(function () {
+    sticky += 1;
+    console.log("St: " + staple);
+    console.log("T: " + trash);
+    console.log("St: " + sticky);
+    eighthQ();
   });
 
 });
